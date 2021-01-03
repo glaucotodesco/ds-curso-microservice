a104a191c51f422f00a44e18f20074c318ff2a70
glauco.todesco <glauco.todesco@facens.br>
2021-01-03 20:47:59 -0300

    Add Eureka Server

---
+++ eurekaserver/src/test/java/com/cursoms/eurekaserver/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoms.eurekaserver.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
